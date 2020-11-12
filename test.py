e38cbc9fd8a4e35757da2cd6767d8f40215d5539
Wahyu Ardiansyah <wahyuardi027@gmail.com>
2020-11-12 11:35:21 +0700

    choose file test

--- test.py
+++ test.py
@@ -8,19 +8,29 @@ Created on Thu Nov 12 10:16:32 2020
 import training
 import os
 import numpy as np
+import logging
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
-model_dir = 'model/model.h5'
-base_dir = '/Users/ARDI/Documents/Python Scripts/ML/data/gender'    
+logging.getLogger('tensorflow').setLevel(logging.CRITICAL)
+logging.getLogger('training').setLevel(logging.CRITICAL)
+model_dir = 'model/model.h5'   
 
 def start_test():
-    model = training.create_model(summary=False)
-    model.load_weights(model_dir)
-    fr, sound = training.read(os.path.join(base_dir, 'fepi_0001.wav'))
-    classes = model.predict(np.vstack([sound[:10000]]), batch_size=10)
-    if classes == 0:
-        print('male')
+    Tk().withdraw()
+    filename = askopenfilename()
+    if filename != '':
+        print(filename)
+        model = training.create_model(summary=False)
+        model.load_weights(model_dir)
+        fr, sound = training.read(filename)
+        classes = model.predict(np.vstack([sound[:10000]]), batch_size=10)
+        if classes == 0:
+            print('male')
+        else:
+            print('female')
     else:
-        print('female')
+        print('Tidak ada file yang dipilih')
         
 if (os.path.isfile(model_dir)):
     start_test()
